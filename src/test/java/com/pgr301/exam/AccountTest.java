0e4c51a8c321e3de173ec54c86af140c44be756d
l4rma <lars.magelssen@gmail.com>
2021-11-24 18:50:58 +0100

    added a test

---
+++ src/test/java/com/pgr301/exam/AccountTest.java
@@ -0,0 +1,21 @@
+package com.pgr301.exam;
+
+import com.pgr301.exam.model.Account;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AccountTest {
+
+    @Test
+    void shouldSetBalance() {
+        BigDecimal balance = BigDecimal.valueOf(50);
+
+        Account a = new Account();
+        a.setBalance(balance);
+
+        assertEquals(a.getBalance(), balance);
+    }
+}
