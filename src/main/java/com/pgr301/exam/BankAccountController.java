2a2f8d7671fe3d136a290d28fca4d994f870c063
l4rma <lars.magelssen@gmail.com>
2021-11-25 19:56:36 +0100

    added names to @Timed and removed meterRegistry

--- src/main/java/com/pgr301/exam/BankAccountController.java
+++ src/main/java/com/pgr301/exam/BankAccountController.java
@@ -3,7 +3,6 @@ package com.pgr301.exam;
 import com.pgr301.exam.model.Account;
 import com.pgr301.exam.model.Transaction;
 import io.micrometer.core.annotation.Timed;
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -11,35 +10,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static java.math.BigDecimal.*;
 import static java.util.Optional.ofNullable;
 
 @RestController
-public class BankAccountController implements ApplicationListener<ApplicationReadyEvent> {
-
-    @Autowired
-    private MeterRegistry meterRegistry;
+public class
+BankAccountController implements ApplicationListener<ApplicationReadyEvent> {
 
     @Autowired
     private BankingCoreSystmeService bankService;
 
-    @Timed
+    @Timed("transfer_money")
     @PostMapping(path = "/account/{fromAccount}/transfer/{toAccount}", consumes = "application/json", produces = "application/json")
     public void transfer(@RequestBody Transaction tx, @PathVariable String fromAccount, @PathVariable String toAccount) {
         bankService.transfer(tx, fromAccount, toAccount);
     }
 
-    @Timed
+    @Timed("update_account")
     @PostMapping(path = "/account", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Account> updateAccount(@RequestBody Account a) {
         bankService.updateAccount(a);
         return new ResponseEntity<>(a, HttpStatus.OK);
     }
 
-    @Timed
+    @Timed("get_account")
     @GetMapping(path = "/account/{accountId}", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Account> balance(@PathVariable String accountId) {
         Account account = ofNullable(bankService.getAccount(accountId)).orElseThrow(AccountNotFoundException::new);
